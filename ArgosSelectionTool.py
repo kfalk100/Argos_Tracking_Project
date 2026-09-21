111b7bb09b3f265392f0bd0c03d0361b391721e4
kfalk100 <kyle.falk@duke.edu>
2026-09-21 12:35:09 -0400

    Initial commit of Argos Selection Tool script

---
+++ ArgosSelectionTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ArgosSelectionTool.py
+#
+# Description: Reads in an Argos tracking data file and allows
+#   the user to identify the tracked sitings found within a 
+#   specified bounding box.
+#
+# Author: Kyle Falk (kyle.falk@duke.edu)
+# Date:   Fall 2026
+#--------------------------------------------------------------
