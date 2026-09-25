3e92e71f1529ea9c2b2faa195a14510f602f981a
kfalk100 <kyle.falk@duke.edu>
2026-09-25 16:02:34 -0400

    Task 7 Commit

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -9,7 +9,15 @@
 # Date:   Fall 2026
 #--------------------------------------------------------------
 
-# Create the geogrpahic selection box
+# Create the geographic selection box
+the_box = {
+    'x_min': 34.00,
+    'y_min': -76.0,
+    'x_max': 34.50,
+    'y_max': -75.00
+}
+
+# Create the geographic selection box
 the_box = {
     'x_min': 34.00,
     'y_min': -76.0,
@@ -21,12 +29,14 @@ the_box = {
 file_name = 'data/raw/Satellite tracking of black-capped petrels 2019-argos.csv'
 
 #Read the contents of the file into a list of lines
-with open(file_name,'r') as f:
-    #Read contents of file into a list
-	line_list = f.readlines()
+f = open(file_name,'r')
+#Read the headerline 
+headerLine = f.readline()
+#Read contents of one line
+lineString = f.readline()
 
 #Pretend we read one line of data from the file
-for lineString in line_list[1:]:
+while lineString != "":
 
     # Use the split command to parse the items in lineString into a list object
     line_data = lineString.split(',')
@@ -35,9 +45,11 @@ for lineString in line_list[1:]:
     event_id = line_data[0]   # Argos tracking event ID ("event-id")
     timestamp = line_data[2]  # Observation date ("timestamp")
     lc  = line_data[14]        # Observation location class ("argos:lc")
-    if lc not in ['"1"','"2"','"3"']:  continue
-    lat = float(line_data[4])        # Observation latitude  ("location-lat")
-    lon = float(line_data[3])        # Observation longitude ("location-lon")
+    if lc not in ['"1"','"2"','"3"']:
+        lineString = f.readline()  
+        continue
+    lat = float(line_data[3])        # Observation latitude  ("location-lat")
+    lon = float(line_data[4])        # Observation longitude ("location-lon")
     tag_id = line_data[-3]     # Tag identifier ("tag-local-identifier")
     
     #Evaluate latitude and longitude conditions
@@ -49,3 +61,9 @@ for lineString in line_list[1:]:
         print(f'Record {event_id}: {tag_id} was IN the box at {timestamp}')
     else:
         print(f'Record {event_id}: {tag_id} was NOT IN the box at {timestamp}')
+
+    #Update line string
+    lineString = f.readline()
+
+#Close the file
+f.close()
